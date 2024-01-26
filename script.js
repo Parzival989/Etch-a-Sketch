7ed8ea5f7051681fb38ecff41f02e782b07a8af0
Parzival989 <julian.hoegel09@gmail.com>
2024-01-26 02:32:30 +0100

    starting

--- script.js
+++ script.js
@@ -19,9 +19,32 @@ console.log(value);
 applyButton.addEventListener("click", () => {
   // get format
   let quadrat = slider.value;
+  // Create container div
+  let containerWidth = sketch.clientWidth;
+  let containerHeight = containerWidth;
+  let packageWidth = containerWidth / quadrat;
+  let packageHeight = packageWidth;
+
+  console.log(containerWidth);
+
+  // Create Child container
+  let childContainer = document.createElement("div");
+  childContainer.style.width = `${containerWidth}px`;
+  childContainer.style.height = `${containerHeight}px`;
+  childContainer.classList.add("border");
+
+  // Create Fields of the child container
+  let newDiv = document.createElement("div");
+  newDiv.style.width = `${packageWidth}px`;
+  newDiv.style.height = `${packageHeight}px`;
+  newDiv.classList.add("border");
+
   // inside Div
   for (let i = 0; i < quadrat; i++) {
-    sketch.appendChild;
-    for (let j = 0; j < quadrat; j++) {}
+    //Container
+    sketch.appendChild(childContainer);
+    for (let j = 0; j < quadrat; j++) {
+      // Child of container
+    }
   }
 });
